b1d2381d0b7a5718db403a609a20dc99e67755b5
Carlos <711carlosalva771@gmail.com>
2024-09-01 19:23:57 -0300

    "Inicio del proyecto"

---
+++ src/main/java/unse/sysbedel/SysBedel.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package unse.sysbedel;
+
+/**
+ *
+ * @author carlos
+ */
+public class SysBedel {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
