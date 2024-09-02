0350d3bbd4bd57d979fa3bfbdae06787591663bf
bruno <bruno@NETGOB-redes>
2024-09-02 15:04:36 -0300

    Unos cambios en paquetes y prueba en mi clase

---
+++ src/main/java/unse/sysbedel/ClasePruebaBruno.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package unse.sysbedel;
+
+/**
+ *
+ * @author bruno
+ */
+public class ClasePruebaBruno {
+    private String miNombre;
+    private String miApellido;
+}
