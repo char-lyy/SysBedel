c4ee8679eb64d5a1a12f26040249749cd699c0a1
Carlos <711carlosalva771@gmail.com>
2024-09-02 15:31:53 -0300

    "Estructuracion del proyecto"

--- src/main/java/principal/Main.java
+++ src/main/java/principal/Main.java
@@ -7,14 +7,8 @@ package principal;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
-        //Este comentario es un commit de Bruno en Ingenieria del Software.
-        System.out.println("Hola Carlitos");
         
-        while (true) {
-            System.out.println("Carlos mas capo");
-            System.out.println("Ñaca ñACA");
-            System.out.println("Que manera de renegar con GitHub !!!");
-        }
+        System.out.println("Hello World!");
+
     }
 }
