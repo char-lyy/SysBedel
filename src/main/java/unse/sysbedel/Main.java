b0fee9c47953653a3e683c95218f62f9546120f5
bruno <bruno@NETGOB-redes>
2024-09-02 14:39:14 -0300

    Puse un sout saludando a Carlos

--- src/main/java/unse/sysbedel/Main.java
+++ src/main/java/unse/sysbedel/Main.java
@@ -12,5 +12,7 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        //Este comentario es un commit de Bruno en Ingenieria del Software.
+        System.out.println("Hola Carlitos");
     }
 }
