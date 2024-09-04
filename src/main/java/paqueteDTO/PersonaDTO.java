2c46f558e5c08e7ff5ef25d03566252e5f056775
Carlos <711carlosalva771@gmail.com>
2024-09-04 12:40:43 -0300

    "Añadida PersonaDTO"

---
+++ src/main/java/paqueteDTO/PersonaDTO.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package paqueteDTO;
+
+/**
+ *
+ * @author carlos
+ */
+public class PersonaDTO {
+
+    private String nombre;
+    private String apellido;
+
+    public PersonaDTO() {
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+}
