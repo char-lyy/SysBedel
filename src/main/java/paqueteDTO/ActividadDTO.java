8bebcd3a7934d83db0e5008ed86040858a274c86
Carlos <711carlosalva771@gmail.com>
2024-09-30 15:22:48 -0300

    "Se agrego la dependencia JCalendar y se agregaron dateChoosers a la vista"

--- src/main/java/paqueteDTO/ActividadDTO.java
+++ src/main/java/paqueteDTO/ActividadDTO.java
@@ -9,6 +9,53 @@ public class ActividadDTO {
     protected FechaDTO fechaFinActividad;
     protected PeriodoActividad periodoActividad;
     protected TipoActividad tipoActividad;
+    protected DiaDeLaSemana diaDeLaSemana;
+
+    public enum DiaDeLaSemana {
+        LUNES, MARTES, MIERCOLES, JUEVES, VIERNES, SABADO, DOMINGO;
+
+        public static DiaDeLaSemana seleccionarDiaDeLaSemana() {
+            int opcion;
+
+            do {
+                System.out.println("Seleccione el dia de la semana para la actividad:");
+                System.out.println("1. LUNES");
+                System.out.println("2. MARTES");
+                System.out.println("3. MIERCOLES");
+                System.out.println("4. JUEVES");
+                System.out.println("5. VIERNES");
+                System.out.println("6. SABADO");
+                System.out.println("7. DOMINGO");
+
+                opcion = GestorEntradaConsola.leerEntero();
+                switch (opcion) {
+                    case 1 -> {
+                        return LUNES;
+                    }
+                    case 2 -> {
+                        return MARTES;
+                    }
+                    case 3 -> {
+                        return MIERCOLES;
+                    }
+                    case 4 -> {
+                        return JUEVES;
+                    }
+                    case 5 -> {
+                        return VIERNES;
+                    }
+                    case 6 -> {
+                        return SABADO;
+                    }
+                    case 7 -> {
+                        return DOMINGO;
+                    }
+                    default ->
+                        System.out.println("Opción inválida. Intente nuevamente.");
+                }
+            } while (true);
+        }
+    }
 
     public enum PeriodoActividad {
         CUATRIMESTRAL,
--- src/main/java/vista/GestionarActividades.java
+++ src/main/java/vista/GestionarActividades.java
@@ -1,17 +1,19 @@
 package vista;
 
+import com.toedter.calendar.JDateChooser;
 import javax.swing.*;
 import java.awt.*;
+import java.util.Vector;
 
 public class GestionarActividades extends JFrame {
 
     public GestionarActividades() {
         setTitle("Gestionar Actividades Académicas");
         setSize(400, 300);
-        setLayout(new GridLayout(3, 2));
+        setLayout(new GridLayout(10, 1));
         setLocationRelativeTo(null);
 
-        JLabel labelActividad = new JLabel("Nombre Actividad:");
+        JLabel labelActividad = new JLabel("Descripcion:");
         JTextField textActividad = new JTextField();
 
         JLabel labelAula = new JLabel("Aula:");
@@ -23,14 +25,47 @@ public class GestionarActividades extends JFrame {
         JButton buttonGuardar = new JButton("Guardar");
         JButton buttonConsultar = new JButton("Consultar Disponibilidad");
 
+        JPanel panelPeriodo = new JPanel(new GridLayout(1, 4));
+
+        JRadioButton button1 = new JRadioButton("Anual");
+        JRadioButton button2 = new JRadioButton("Cuatrimestral");
+        JRadioButton button3 = new JRadioButton("Unico");
+
+        ButtonGroup group = new ButtonGroup();
+        group.add(button1);
+        group.add(button2);
+        group.add(button3);
+
+        panelPeriodo.add(button1);
+        panelPeriodo.add(button2);
+        panelPeriodo.add(button3);
+
+        add(new JLabel("Periodo"));
+        add(panelPeriodo);
+
         add(labelActividad);
         add(textActividad);
+
         add(labelAula);
         add(textAula);
+
         add(labelHorario);
         add(textHorario);
+
+        add(new JLabel("Fecha"));
+        JDateChooser dateChooser = new JDateChooser();
+        add(dateChooser);
+
+        add(new JLabel("Dia: "));
+//        
+        // Array con los días de la semana
+        String[] diasDeLaSemana = {"Lunes", "Martes", "Miércoles", "Jueves", "Viernes", "Sábado", "Domingo"};
+
+        JComboBox<String> comboBoxDiasSemana = new JComboBox<>(diasDeLaSemana);
+        
+        add(comboBoxDiasSemana);
         add(buttonGuardar);
         add(buttonConsultar);
+
     }
 }
-
