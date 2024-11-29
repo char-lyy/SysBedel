aea2e4da0bf99ea6bc10a9106625e8f3e778acbd
Carlos <711carlosalva771@gmail.com>
2024-11-29 00:10:03 -0300

    Correcciones menores en la interfaz de recuperacion de contraseña.

--- src/main/java/vista/panels/PanelCambiarContraseña.java
+++ src/main/java/vista/panels/PanelCambiarContraseña.java
@@ -7,23 +7,25 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 import paqueteDAO.CuentaDAO;
 import services.ServicioRecuperacion;
+import vista.VentanaPrincipal;
+import vista.frames.FrameLogin;
 
 public class PanelCambiarContraseña extends JPanel {
 
     private JTextField codigoField;
-    private JTextField nuevaContrasenaField;
+    private JPasswordField nuevaContrasenaField;
     private JButton cambiarButton;
     private JLabel mensajeLabel;
     private JFrame frame;
     private ServicioRecuperacion servicioRecuperacion;
-    
+
     public PanelCambiarContraseña(JFrame frame, ServicioRecuperacion servicioRecuperacion) {
-        
+
         this.frame = frame;
 
         // Crear componentes
         codigoField = new JTextField(20);
-        nuevaContrasenaField = new JTextField(20);
+        nuevaContrasenaField = new JPasswordField(20);
         cambiarButton = new JButton("Cambiar Contraseña");
         mensajeLabel = new JLabel("");
 
@@ -45,7 +47,13 @@ public class PanelCambiarContraseña extends JPanel {
                         } catch (Exception ex) {
                             Logger.getLogger(PanelCambiarContraseña.class.getName()).log(Level.SEVERE, null, ex);
                         }
-                        mensajeLabel.setText("Contraseña cambiada con éxito.");
+                        JOptionPane.showMessageDialog(null, "Te hemos enviado un código de recuperación al correo.");
+                        
+                        frame.dispose();
+                        
+                        FrameLogin frameLogin = new FrameLogin();
+                        
+                        frameLogin.setVisible(true);
                     } else {
                         mensajeLabel.setText("Código inválido.");
                     }
